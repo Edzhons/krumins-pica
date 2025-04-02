02de34be4658db91f78a95af9367c5e5ed365a20
Edzhons <edzuskrumins13@gmail.com>
2025-04-02 10:23:42 +0300

    Izveidota pirmā funkcionālā JOptionPane ievade

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Kukuu!");
+
+        String izvele;
+        String[] darbibas = {"Margarita", "Salami", "Hawaii", "Peperoni (asa)", "Veģetārā"};
+        int izvelesIndekss = 0;
+        do{
+            izvele = (String) JOptionPane.showInputDialog(
+                null,
+                "Izvēlies jautājumu:",
+                "Darbības izvēle",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                darbibas,
+                darbibas[0]
+            );
+            izvelesIndekss = Arrays.asList(darbibas).indexOf(izvele);
+
+            if (izvele == null || izvele.equals("Iziet")) {
+                JOptionPane.showMessageDialog(null, "Programma apturēta.");
+                break;
+            }
+        }while(izvelesIndekss != 3);
     }
 }
