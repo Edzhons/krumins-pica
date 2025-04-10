cec94eb2a7189553d079e11e3f8be27dfc3bf325
Edzhons <edzuskrumins13@gmail.com>
2025-04-10 21:58:20 +0300

    App.java -> Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import javax.swing.JOptionPane;
 
-public class App {
+public class Main {
     public static void main(String[] args) throws Exception {
 
         String[] darbibas = {"Banka", "Picērija", "Iziet"};
