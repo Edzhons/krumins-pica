fb3e1c0b474944e1b33adcd8f0050f80699d6347
Edzhons <edzuskrumins13@gmail.com>
2025-04-13 20:49:16 +0300

    Jauna Klients klase

--- src/Dati.java
+++ src/Dati.java
@@ -6,4 +6,5 @@ public class Dati {
     public static List<Banka> bankas = new ArrayList<>();
     public static List<String> piedevas = new ArrayList<>();
     public static List<String> merces = new ArrayList<>();
+    public static List<String> klienti = new ArrayList<>();
 }
---
+++ src/Klients.java
@@ -0,0 +1,18 @@
+public class Klients {
+    private String lietotajvards;
+    private String parole;
+
+    public Klients(String lietotajvards, String parole) {
+        this.lietotajvards = lietotajvards;
+        this.parole = parole;
+    }
+
+    public String getLietotajvards() {
+        return lietotajvards;
+    }
+
+    public String getParole() {
+        return parole;
+    }
+
+}
